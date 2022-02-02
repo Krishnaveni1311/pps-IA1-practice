eeaef7145b2ebad7e2e329fbb31235dc3f99b1b8
Krishnaveni1311 <94587768+Krishnaveni1311@users.noreply.github.com>
2022-02-02 09:01:08 +0000

    executed

--- p2original.c
+++ p2original.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-int input()
+int main()
 {
   int a,b,c;
-  printf("Enter three numbers\n");
-  scanf("%d%d%d",&a,)
+  printf("Enter three numbers");
+  scanf("%d%d%d",&a,&b,&c);
+  if((a>b)&&(a>c))
+  printf("A is big \n");
+  else if((b>a)&&(b>c))
+  printf("B is big \n");
+else
+printf("C is big \n");
+return 0;
 }
